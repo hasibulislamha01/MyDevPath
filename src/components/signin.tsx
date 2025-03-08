db2dddb22c1a06ca3b45c369940829cfe5547b31
hasibul islam <hasibulislamha01@gmail.com>
2025-03-08 12:37:44 +0600

    google login added using auth js

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,7 +1,12 @@
 
-import { signIn } from "../../auth"
- 
-export default function SignIn() {
+import { signIn, auth } from "../../auth"
+
+export default async function SignIn() {
+  const sesssion = await auth()
+  const user = sesssion?.user
+  console.log(sesssion)
+  console.log(user)
+
   return (
     <form
       action={async () => {
@@ -9,7 +14,12 @@ export default function SignIn() {
         await signIn("google")
       }}
     >
-      <button type="submit">Signin with Google</button>
+      {
+        user ?
+          <button>Sign Out</button>
+          :
+          <button type="submit">Signin with Google</button>
+      }
     </form>
   )
 } 
